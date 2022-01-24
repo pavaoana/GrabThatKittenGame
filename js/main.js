b167dab3465637f481a7bea57033cbeb2e92169a
pavaoana <anapavao2@gmail.com>
2022-01-24 17:05:31 +0000

    cats appear at random positions and disappear after a while

--- js/main.js
+++ js/main.js
@@ -1,62 +1,37 @@
-// Instructions Button:
-function expandFunction() {
-  let collapsible = document.getElementById("instructions");
-  if (collapsible.style.display === "none") {
-    collapsible.style.display = "block";
-  } else {
-    collapsible.style.display = "none";
-  } // not really working; the instructions shouldn't appear if the button has not been clicked
-}
-
 // Game Logic:
 class Game {
   constructor() {
     this.kittensArr = [];
-    this.kittenCounter = 0; // necessary?
   }
 
   start() {
-    const startButton = document.querySelector("#start-button");
-    startButton.addEventListener("click", () => {
-      startButton.style.display = "none";
-    });
+    setInterval(() => {
+      const kitten = new Kitten();
+      this.kittensArr.push(kitten);
+      kitten.domElement = this.createDomElm(kitten);
+      this.drawDomElm(kitten);
+    }, 1000);
 
-    kittensAppear();
+    setInterval(() => {
+      const kittenToRemove = this.kittensArr.shift();
+      console.log(kittenToRemove);
+      kittenToRemove.domElement.remove();
+    }, 1800);
   }
 
-  /* using a setInterval() for this????:
-
-  const newKitten = new Kitten();
-  this.kittensArr.push(newKitten);
-  newKitten.domElement = this.createDomElm(newKitten);
-  this.drawDomElm(newKitten);
-
-  this.kittensArr.forEach((kitten) => {
-  kitten.kittensAppear();
-  this.drawDomElm(kitten);
-  });
-
-  */
-
-  /* 
-    createDomElm(instance){
-    const htmlTag = document.createElement("div"); 
-    htmlTag.className = instance.className; 
+  createDomElm(instance) {
+    const htmlTag = document.createElement("div"); // create html element (not added to the dom yet)
+    htmlTag.className = instance.className; // add class (so that we can reuse this function to create different types of elements in the dom, eg. player, obstacles....)
     htmlTag.style.width = instance.width + "vw";
     htmlTag.style.height = instance.height + "vh";
-    const board = document.getElementById("board"); 
-    board.appendChild(htmlTag); 
+    const board = document.getElementById("game-board"); // get a reference to the parent container
+    board.appendChild(htmlTag); // append the element to the dom
     return htmlTag;
   }
-  */
-
-  /* 
-  drawDomElm(kitten) {
-    kitten.domElement = kitten.positionX + "vw";
+  drawDomElm(instance) {
+    instance.domElement.style.left = instance.positionX + "vw";
+    instance.domElement.style.bottom = instance.positionY + "vh";
   }
-  */
-
-  endGame() {}
 }
 
 class Kitten {
@@ -64,19 +39,41 @@ class Kitten {
     this.className = "kitten";
     this.width = 5;
     this.height = 10;
-    // this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
-    // this.positionY = Math.floor(Math.random() * (100 - this.width + 1));
-  }
+    this.positionX = Math.floor(Math.random() * (100 - this.width + 1)); // random number between 0 and 100-width
+    this.positionY = Math.floor(Math.random() * (100 - this.height + 1)); // random number between 0 and 100-height
+    this.domElement = null;
 
-  kittensAppear() {}
+    console.log(
+      "a kitten was created on position... " +
+        this.positionX +
+        " , " +
+        this.positionY
+    );
+  }
 }
 
 const game = new Game();
 game.start();
 
+//
 /* 
 See if it helps:
 https://habr.com/en/sandbox/144776/
 https://www.geeksforgeeks.org/design-hit-the-mouse-game-using-html-css-and-vanilla-javascript/
 https://stackoverflow.com/questions/24249314/simple-2d-game-using-a-mouse
 */
+
+/* // Instructions Button:
+function expandFunction() {
+  let collapsible = document.getElementById("instructions");
+  if (collapsible.style.display === "none") {
+    collapsible.style.display = "block";
+  } else {
+    collapsible.style.display = "none";
+  } // not really working; the instructions shouldn't appear if the button has not been clicked
+} */
+
+/* const startButton = document.querySelector("#start-button");
+    startButton.addEventListener("click", () => {
+      startButton.style.display = "none";
+    });*/
